026363ebf672b47504ac7bd331182b2d8efa5a53
Adas Petrovas <adas.petrovas@gmail.com>
2023-02-20 13:05:19 +0200

    Prettier, auth package

--- apps/triptrip/src/App.tsx
+++ apps/triptrip/src/App.tsx
@@ -1,20 +1,15 @@
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  getRedirectResult,
-  GoogleAuthProvider,
-  signInWithRedirect,
-} from "firebase/auth";
-import { useEffect } from "react";
+import { initializeApp } from 'firebase/app';
+import { getAuth, getRedirectResult, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { useEffect } from 'react';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "triptrip-a7794.firebaseapp.com",
-  projectId: "triptrip-a7794",
-  storageBucket: "triptrip-a7794.appspot.com",
-  messagingSenderId: "454613313754",
-  appId: "1:454613313754:web:701c3578b0c454d0e35de3",
-  measurementId: "G-LVCZHTXJ0Q",
+  authDomain: 'triptrip-a7794.firebaseapp.com',
+  projectId: 'triptrip-a7794',
+  storageBucket: 'triptrip-a7794.appspot.com',
+  messagingSenderId: '454613313754',
+  appId: '1:454613313754:web:701c3578b0c454d0e35de3',
+  measurementId: 'G-LVCZHTXJ0Q'
 };
 
 // Initialize Firebase
@@ -27,14 +22,14 @@ function App() {
   useEffect(() => {
     getRedirectResult(auth)
       .then((result) => {
-        console.log("DONE", result);
+        console.log('DONE', result);
       })
       .catch(console.error);
   }, []);
 
   const handleSignIn = () => {
     signInWithRedirect(auth, provider).catch((error) => {
-      console.error("ERR", error);
+      console.error('ERR', error);
     });
   };
 
